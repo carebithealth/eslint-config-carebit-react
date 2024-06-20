eee772462cf04a8d8b2db7c89b935d12181e166c
Nick Major <nick.major@carebit.co>
2024-06-20 14:59:40 +0100

    test

--- recommended.js
+++ recommended.js
@@ -26,7 +26,7 @@ const restrictedGlobals = require("confusing-browser-globals");
 module.exports = {
   extends: [require.resolve("./base")],
 
-  plugins: ["import", "jsx-a11y", "react-hooks"],
+  plugins: ["import", "flowtype", "jsx-a11y", "react-hooks"],
 
   overrides: [
     {
